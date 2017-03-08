3b58a3586c7204dc20fe4a5410f0f1bb3d8a4d40
Destin Moulton <destin.moulton@gmail.com>
2017-03-07 18:08:37 -0700

    Added .gitignore

---
+++ gui/fadingwidget.py
@@ -0,0 +1,49 @@
+"""A QT widget to create the buttons for the i3Void QT window.
+"""
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+class ButtonWidget(QWidget):
+
+    def __init__(self, parent=None):
+        """Initialize the buttons, their click event handlers, and their position.
+        """
+        
+        super(QWidget, self).__init__(parent)
+        self._parent = parent
+        self._current_color_hex = "#2e2e2e"
+        
+        self.color_button = QPushButton('Color', self)
+        self.color_button.clicked.connect(self.showColorPicker)
+        self.color_button.resize(self.color_button.sizeHint())
+
+        self.close_button = QPushButton(' X ', self)
+        self.close_button.clicked.connect(self._parent.closeWindow)
+        self.close_button.resize(self.close_button.sizeHint())
+        
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.color_button)
+        hbox.addWidget(self.close_button)
+
+        vbox = QVBoxLayout()
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+        
+        self.setLayout(vbox)
+
+        self.repaint()
+
+    def showColorPicker(self):
+        """Show the color picker when the Color button is clicked.
+        """
+        
+        color = QColor(0,0,0)
+        color.setNamedColor(self._current_color_hex)
+        selectedColor = QColorDialog.getColor(color)
+
+        if selectedColor.isValid():
+            self._current_color_hex = selectedColor.name()
+            self._parent.setWindowColor(self._current_color_hex)
+        
