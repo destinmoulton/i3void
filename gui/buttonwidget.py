e507e8358c10d2b90ad167826ffe7c3963928446
Destin Moulton <destin.moulton@gmail.com>
2019-11-20 09:24:40 -0700

    Updated to qt5

--- gui/buttonwidget.py
+++ gui/buttonwidget.py
@@ -1,8 +1,10 @@
 """A QT widget to create the buttons for the i3Void QT window.
 """
 
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+from PyQt5.QtWidgets import QApplication, QColorDialog, QWidget, QPushButton, QHBoxLayout, QVBoxLayout
+
+from PyQt5.QtGui import QColor
+
 
 class ButtonWidget(QWidget):
     """A QT Widget that:
@@ -13,11 +15,11 @@ class ButtonWidget(QWidget):
     def __init__(self, parent=None):
         """Initialize the buttons, their click event handlers, and their position.
         """
-        
+
         super(QWidget, self).__init__(parent)
         self._parent = parent
         self._current_color_hex = "#2e2e2e"
-        
+
         self.color_button = QPushButton('Color', self)
         self.color_button.clicked.connect(self.showColorPicker)
         self.color_button.resize(self.color_button.sizeHint())
@@ -25,7 +27,7 @@ class ButtonWidget(QWidget):
         self.close_button = QPushButton(' X ', self)
         self.close_button.clicked.connect(self._parent.closeWindow)
         self.close_button.resize(self.close_button.sizeHint())
-        
+
         hbox = QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.color_button)
@@ -34,7 +36,7 @@ class ButtonWidget(QWidget):
         vbox = QVBoxLayout()
         vbox.addLayout(hbox)
         vbox.addStretch(1)
-        
+
         self.setLayout(vbox)
 
         self.repaint()
@@ -42,12 +44,11 @@ class ButtonWidget(QWidget):
     def showColorPicker(self):
         """Show the color picker when the Color button is clicked.
         """
-        
-        color = QColor(0,0,0)
+
+        color = QColor(0, 0, 0)
         color.setNamedColor(self._current_color_hex)
         selectedColor = QColorDialog.getColor(color)
 
         if selectedColor.isValid():
             self._current_color_hex = selectedColor.name()
             self._parent.setWindowColor(self._current_color_hex)
-        
--- i3void.py
+++ i3void.py
@@ -14,19 +14,22 @@ __version__ = "0.2.0"
 
 import sys
 
-from PyQt4 import QtGui
-from PyQt4 import QtCore
+from PyQt5.QtCore import QEvent
+from PyQt5.QtGui import QKeySequence
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QShortcut, QWidget
 
 from gui.buttonwidget import ButtonWidget
 
-class i3Void(QtGui.QMainWindow):
+
+class i3Void(QMainWindow):
     """Create the QT window application.
     """
-    
+
     def __init__(self):
         """Set the parameters for the QT window.
         """
-        
+
         super(i3Void, self).__init__()
 
         self.setWindowTitle('i3Void')
@@ -41,44 +44,44 @@ class i3Void(QtGui.QMainWindow):
         self.installEventFilter(self)
 
         # Setup Ctrl+q to exit
-        QtGui.QShortcut(QtGui.QKeySequence("Ctrl+Q"), self, self.close)
+        QShortcut(QKeySequence("Ctrl+Q"), self, self.close)
 
     def eventFilter(self, source, event):
         """Hide and show the buttons when the window is focused.
         """
-        
-        if event.type() == QtCore.QEvent.WindowActivate:
-            self.buttonWidget.show();
-        elif event.type()== QtCore.QEvent.WindowDeactivate:
-            self.buttonWidget.hide();
-        return QtGui.QWidget.eventFilter(self, source,event)
+
+        if event.type() == QEvent.WindowActivate:
+            self.buttonWidget.show()
+        elif event.type() == QEvent.WindowDeactivate:
+            self.buttonWidget.hide()
+        return QWidget.eventFilter(self, source, event)
 
     def setWindowColor(self, new_color):
         """Change the color of the window.
         """
-        
+
         self.setStyleSheet('QMainWindow{background-color: '+new_color+';}')
 
     def closeWindow(self):
         """Close the window.
         """
-        
+
         self.close()
-        
+
     def closeEvent(self, event):
         """Accept a close event.
         """
-        
+
         event.accept()
 
-        
-if __name__ == '__main__':        
+
+if __name__ == '__main__':
     """Start the window when this script is run.
     """
-    
+
     # Pass any args to the QT application
-    app = QtGui.QApplication(sys.argv)
-    
+    app = QApplication(sys.argv)
+
     # Instantiate the i3Void window
     sq = i3Void()
 
